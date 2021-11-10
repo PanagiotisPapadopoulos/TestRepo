fcb4f3d3a5e3672f2be2c3c10b361876b86f0311
panagiotispapadopoulos <panagiotis.papadopoulos@hiziggy.com>
2021-11-10 09:54:06 +0200

    +Added Roles

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using HotelListing.Configurations.Entities;
 
 namespace HotelListing.Data
 {
@@ -20,55 +21,9 @@ namespace HotelListing.Data
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<Country>().HasData(
-                new Country
-                {
-                    Id = 1,
-                    Name = "Jamaica",
-                    ShortName = "JM"
-                },
-                new Country
-                {
-                    Id = 2,
-                    Name = "Bahamas",
-                    ShortName = "BS"
-                },
-                new Country
-                {
-                    Id = 3,
-                    Name = "Cayman Island",
-                    ShortName = "CI"
-                }
-
-            );
-
-            builder.Entity<Hotel>().HasData(
-                new Hotel
-                {
-                    Id = 1,
-                    Name = "Sandals Resort and Spa",
-                    Address = "Negril",
-                    CountryId = 1,
-                    Rating = 4.5
-                },
-                new Hotel
-                {
-                    Id = 2,
-                    Name = "Comfort Suites",
-                    Address = "George Town",
-                    CountryId = 3,
-                    Rating = 4.3
-                },
-                new Hotel
-                {
-                    Id = 3,
-                    Name = "Grand Palladium",
-                    Address = "Nassua",
-                    CountryId = 2,
-                    Rating = 4
-                }
-
-            );
+            builder.ApplyConfiguration(new RoleConfiguration());
+            builder.ApplyConfiguration(new CountryConfiguration());
+            builder.ApplyConfiguration(new HotelConfiguration());
         }
 
     }
---
+++ Migrations/20211110075310_AddDefaultRoles.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class AddDefaultRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "b9001fdb-2301-433c-a101-a0744d313092", "0bc4193b-87fb-4e06-a940-0eeb38153926", "User", "USER" },
+                    { "83abcbce-db09-4cbe-8031-dfbdeb0e2adc", "2a3d19b0-aad7-4c74-9fbe-fa4d37ba244d", "Administrator", "ADMINISTRATOR" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ShortName",
+                value: "JN");
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Name", "Rating" },
+                values: new object[] { "Grand palldium", 4.2999999999999998 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "83abcbce-db09-4cbe-8031-dfbdeb0e2adc");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b9001fdb-2301-433c-a101-a0744d313092");
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ShortName",
+                value: "JM");
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Name", "Rating" },
+                values: new object[] { "Grand Palladium", 4.0 });
+        }
+    }
+}
